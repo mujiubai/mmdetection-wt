9f01e4010acd150aceb0fc13eb3a722506219548
mujiubai <1057378931@qq.com>
2024-03-18 19:24:51 +0800

    faster-rcnn

--- configs/faster_rcnn/faster-rcnn_r50_fpn_1x_coco.py
+++ configs/faster_rcnn/faster-rcnn_r50_fpn_1x_coco.py
@@ -3,3 +3,45 @@ _base_ = [
     '../_base_/datasets/coco_detection.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
+
+dataset_type = 'CocoDataset'
+data_root = 'data/coco/'
+# metainfo = {
+#     'classes': ('xyr', 'rmz', 'qf',"zj","hnpt","bt","fzthxm","hhytj"),
+#     'palette': [
+#         (220, 20, 60), (119, 11, 32), (0, 0, 142), (0, 0, 230), (106, 0, 228),
+#          (0, 60, 100), (0, 80, 100), (0, 0, 70)
+#     ]
+# }
+class_name=('xyr', 'rmz', 'qf',"zj","hnpt","bt","fzthxm","hhytj")
+num_classes=len(class_name)
+metainfo=dict(classes=class_name)
+
+
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type=dataset_type,
+        metainfo=dict(classes=class_name),
+        data_root=data_root,
+        ann_file='annotations/instances_train2017.json',
+        data_prefix=dict(img='train2017/'),
+        filter_cfg=dict(filter_empty_gt=True, min_size=32),))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        metainfo=dict(classes=class_name),
+        data_root=data_root,
+        ann_file='annotations/instances_val2017.json',
+        data_prefix=dict(img='val2017/'),
+        test_mode=True,))
+test_dataloader = val_dataloader
--- mmdet/evaluation/functional/class_names.py
+++ mmdet/evaluation/functional/class_names.py
@@ -72,6 +72,9 @@ def imagenet_vid_classes() -> list:
 def coco_classes() -> list:
     """Class names of COCO."""
     return [
+        'xyr', 'rmz', 'qf',"zj","hnpt","bt","fzthxm","hhytj"
+    ]
+    return [
         'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train',
         'truck', 'boat', 'traffic_light', 'fire_hydrant', 'stop_sign',
         'parking_meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep',
